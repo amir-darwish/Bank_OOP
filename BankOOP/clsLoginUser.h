b65db05e539b94896d665d65027d27ec385d2aa2
Mohamed Amir DARWISH <mhmad1999.sy@gmail.com>
2025-04-24 12:28:34 +0200

    add login screen

---
+++ BankOOP/clsLoginUser.h
@@ -0,0 +1,48 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include "clsString.h"
+#include "clsScreen.h"
+#include "clsUser.h"
+#include <iomanip>
+#include "clsInputValidate.h"
+#include "Global.h"
+#include "clsMainScreen.h"
+
+
+class clsLoginUser:protected clsScreen
+{
+
+private :
+	static void _Login() {
+
+		bool LoginFaild = false;
+		string Username = "";
+		string Password = "";
+		do {
+			if (LoginFaild) {
+				cout << "\nInvlaid Username/Password!\n\n";
+			}
+			cout << "Please Enter your Username : ";
+			Username = clsInputValidate::ReadString();
+
+			cout << "Please Enter your Password : ";
+			Password = clsInputValidate::ReadString();
+
+			CurrentUser = clsUser::Find(Username, Password);
+			
+			LoginFaild = CurrentUser.IsEmpty();
+
+		}while (LoginFaild);
+
+		clsMainScreen::ShowMainMenue();
+	}
+public:
+
+	static void ShowLoginScreen() {
+		system("cls");
+		_DrawScreenHeader("\t  Login Screen");
+		_Login();
+	}
+};
+
